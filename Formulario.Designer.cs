d0f5321cb0112609aa0cb962373cdc4b98688df1
Lorena Noemy Bermudez Rodriguez <lorenabermudezro@gmail.com>
2022-07-29 20:47:03 -0600

    Agregación de una etiqueta(Label)

--- Formulario.Designer.cs
+++ Formulario.Designer.cs
@@ -30,6 +30,7 @@ namespace Semana2_primer_proyecto_visual_c_sharp_formulario
         private void InitializeComponent()
         {
             this.btnClickThis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnClickThis
@@ -41,21 +42,33 @@ namespace Semana2_primer_proyecto_visual_c_sharp_formulario
             this.btnClickThis.Text = "Click this";
             this.btnClickThis.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // Formulario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 328);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClickThis);
             this.Name = "Formulario";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label label1;
     }
 }
 
